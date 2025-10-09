d068ebce4b77dbab121d61f54a21c299c876bbce
leonardoschulze <leonardo_schulze@estudante.sesisenai.org.br>
2025-10-09 10:05:11 -0300

    Conexão com rede WiFi

---
+++ s2/2/2.ino
@@ -0,0 +1,19 @@
+#include <WiFi.h>
+
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Conectando ao WiFi"); //apresenta a mensagem na tela
+  WiFi.begin(SSID,PASS); //tenta conectar na rede
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print("."); // mostra "....."
+    delay(200);
+  }
+  Serial.println("\nConectado com Sucesso!");
+}
+
+void loop() {
+
+}
