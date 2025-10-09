d3b638790f17c15db3b90271f53b5e959a4205a5
gtrrx <giovanni_l_tonet@estudante.sesisenai.org.br>
2025-10-09 10:07:25 -0300

    conexão com rede WIFI

--- trem/trem.ino
+++ trem/trem.ino
@@ -1,6 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
 
+const String SSID = "nome";
+const String PASS = "senha";
+
+
+void setup() {
+  serial.begin(115200);
+  serial.println("Conectando ao WIFI");
+  WiFi.begin(SSID,PASS);
+
+  while(WiFi.status () != WL_CONNECTED){
+    serial.print(".");
+    delay(100);
+  }
+  serial.println("\nConectado com Sucesso");
 }
 
 void loop() {
